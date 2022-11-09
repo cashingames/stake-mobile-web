c455df7b122fad9ee74cb5a703d8f9d7938ee4d9
Aruna Joy <arunajoy2602@gmail.com>
2022-11-09 18:15:42 +0400

    designing signup profile screen

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,11 +3,16 @@ import styles from './Input.module.scss'
 
 
 
-const Input = ({ inputLabelText, placeholderText, inputType, minLength, maxLength }: any) => {
+const Input = ({ inputLabelText, placeholderText, inputType, 
+    minLength, maxLength, value , onChange
+}: any) => {
     return (
         <div className={styles.inputContainer}>
             <label className={styles.inputLabel}>{inputLabelText}</label>
-            <input className={styles.inputBox} type={inputType} placeholder={placeholderText} minLength={minLength} maxLength={maxLength} />
+            <input className={styles.inputBox} 
+            type={inputType} placeholder={placeholderText} 
+            minLength={minLength} maxLength={maxLength}
+            value = {value} onChange = {onChange} />
         </div>
     )
 }
