13f60b93328dd7047a59597eb0d5a3891e56a1c5
ererobe95 <danielererobe@gmail.com>
2022-11-18 21:11:03 +0100

    dashbord font fixed

--- src/components/TopPlayers/TopPlayers.js
+++ src/components/TopPlayers/TopPlayers.js
@@ -17,26 +17,33 @@ function TopPlayers() {
             </div>
             <div className='leaderBoard'>
                 <div className='positionContainer'>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    <div className='leaderProfile'>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" 
+                    />
                     <p className='leaderName'>Remy</p>
+                    </div>
                     <img src='/images/position3.png'   alt='third position' />
                     <div className='leaderPoints'>
                         <p className='point'>77 pts</p>
                     </div>
                 </div>
                 <div className='positionContainer'>
+                <div className='leaderProfile'>
                     <Avatar>
                         <Person />
                     </Avatar>
                     <p className='leaderName'>James</p>
+                    </div>
                     <img src='/images/position1.png' alt='third position' />
                     <div className='leaderPoints'>
                         <p className='point'>22.4 pts</p>
                     </div>
                 </div>
                 <div className='positionContainer'>
+                <div className='leaderProfile'>
                     <Avatar alt="Sharp" src="/static/images/avatar/1.jpg" />
                     <p className='leaderName'>Sharp</p>
+                    </div>
                     <img src='/images/position2.png' alt='third position' />
                     <div className='leaderPoints'>
                         <p className='point'>63.5 pts</p>
