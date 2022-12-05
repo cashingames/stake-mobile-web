a0bddbbe38aeff4b64e6612311bfaf13d846ece7
ererobe95 <danielererobe@gmail.com>
2022-12-05 10:33:12 +0100

    Home screen  active link implementation

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { IoWalletOutline, IoNotificationsOutline, IoHomeOutline, IoMenuSharp } from "react-icons/io5";
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import DrawerNavigation from '../DrawerNavigation';
 import './AppHeader.scss'
 
@@ -17,48 +17,37 @@ const AppHeader = ({ heading }) => {
     setOpen(false)
   }
 
-
-  let navigate = useNavigate();
-
   const user = useSelector(state => state.auth.user);
-
-  const goToWallet = () => {
-    navigate('/wallet')
-  }
-  const goToDashboard = () => {
-    navigate('/dashboard')
-  }
-  const goToNotifications = () => {
-    navigate('/notifications')
-  }
-
   return (
     <>
-    <div className='appHeaderContainer'>
-      <div className='appHeaderTitle'>
-        <IoMenuSharp className='icon' onClick={handleOpenDrawer}/>
-        <h1 className='title'>{heading}</h1>
+      <div className='appHeaderContainer'>
+        <div className='appHeaderTitle'>
+          <IoMenuSharp className='icon'
+            onClick={handleOpenDrawer} />
+          <h1 className='title'>
+            {heading}</h1>
+        </div>
+        <div className='navlinks'>
+          <NavLink to='/dashboard' className='navlink'>
+            <IoHomeOutline className='icon' />
+            home
+          </NavLink>
+          <NavLink to='/wallet' className='navlink'>
+            <IoWalletOutline className='icon' />
+            wallet
+          </NavLink>
+          <NavLink to='/notifications' className='notification'>
+            <IoNotificationsOutline className='notifyIcon' /> {
+              user.unreadNotificationsCount !== 0 && <div className='notificationNumberCase'>
+                <p className='notificationNumber'>{
+                  user.unreadNotificationsCount
+                }</p>
+              </div>
+            } </NavLink>
+        </div>
       </div>
-      <div className='navlinks'>
-        <div className='navlink' onClick={goToDashboard}>
-          <IoHomeOutline className='icon' />
-          <p className='text'>home</p>
-        </div>
-        <div className='navlink' onClick={goToWallet}>
-          <IoWalletOutline className='icon' />
-          <p className='text'>wallet</p>
-        </div>
-        <div className='notification' onClick={goToNotifications}>
-          <IoNotificationsOutline className='notifyIcon' />
-          {user.unreadNotificationsCount !== 0 &&
-            <div className='notificationNumber'>
-              <p>{user.unreadNotificationsCount}</p>
-            </div>
-          }
-        </div>
-      </div>
-    </div>
-    <DrawerNavigation open={open} closeDrawer={handleCloseDrawer} />
+      <DrawerNavigation open={open}
+        closeDrawer={handleCloseDrawer} />
     </>
   )
 }
