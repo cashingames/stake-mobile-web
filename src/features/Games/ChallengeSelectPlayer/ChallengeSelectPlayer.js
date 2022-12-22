3d99fe99cda11580cb47fd441da629494b06c315
ererobe95 <danielererobe@gmail.com>
2022-12-22 15:53:48 +0100

    challenge selected player screen

--- src/features/Games/ChallengeSelectPlayer/ChallengeSelectPlayer.js
+++ src/features/Games/ChallengeSelectPlayer/ChallengeSelectPlayer.js
@@ -1,8 +1,20 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import './ChallengeSelectedPlayer.scss'
 const ChallengeSelectPlayer = () => {
+    
+    const navigate = useNavigate()
+
+    const navigateHandler = () => {
+        navigate('/dashboard')
+    }
+
     return (
-        <div>iiiii</div>
+        <div className="challengeSelectedPlayer">
+        <p className="csTitle">Not Available</p>
+        <p className="csText">Click on the button below to go back home</p>
+        <button className="csBtn" onClick={navigateHandler}>Home</button>
+        </div>
     )
 }
 export default ChallengeSelectPlayer;
