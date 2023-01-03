881210721d0ed86d9f3ee87600c2b04c8d107905
Aruna Joy <arunajoy2602@gmail.com>
2023-01-04 00:22:39 +0400

    fixing merge conflict

--- src/features/Games/ChallengeStakingBottomSheet/ChallengeStakingBottomSheet.js
+++ src/features/Games/ChallengeStakingBottomSheet/ChallengeStakingBottomSheet.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import './ChallengeStakingBottomSheet.scss'
 
 function ChallengeStakingBottomSheet({onPress,onStake}) {
-    const navigate = useNavigate()
 
    
   return (
