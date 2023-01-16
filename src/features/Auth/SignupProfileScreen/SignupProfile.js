f379689d62a3c2496fcc65e0964f2fe5ed0123eb
oye <oyesola.ogundele@betagy.com>
2023-01-16 17:06:32 +0400

    fixing uncommited code

--- src/features/Auth/SignupProfileScreen/SignupProfile.js
+++ src/features/Auth/SignupProfileScreen/SignupProfile.js
@@ -4,7 +4,7 @@ import AuthTitle from '../../../components/AuthTitle/AuthTitle';
 import { useSelector } from 'react-redux';
 import { registerUser } from '../AuthSlice';
 import { useNavigate } from "react-router-dom";
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
 
 
 import './SignupProfile.scss'
