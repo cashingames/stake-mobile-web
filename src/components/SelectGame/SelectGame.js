77c39b353220c69429a6904aa48c4b0c875701c4
Aruna Joy <arunajoy2602@gmail.com>
2023-02-07 17:35:41 +0400

    fixing deployment issue

--- src/components/SelectGame/SelectGame.js
+++ src/components/SelectGame/SelectGame.js
@@ -33,31 +33,31 @@ console.log(gameMode)
   )
 }
 
-const AvailableMode = ({ gameMode, onPress}) => {
-  const backendUrl = process.env.REACT_APP_API_ROOT_URL;
+// const AvailableMode = ({ gameMode, onPress}) => {
+//   const backendUrl = process.env.REACT_APP_API_ROOT_URL;
 
-  return (
-    <div
-      onClick={onPress}
-      className={`${gameMode.name === 'EXHIBITION' || gameMode.name === 'CHALLENGE' ? 'exhibition-gameCard': 'gameCard'}`}
-      style={{ backgroundColor: gameMode.bgColor }}
+//   return (
+//     <div
+//       onClick={onPress}
+//       className={`${gameMode.name === 'EXHIBITION' || gameMode.name === 'CHALLENGE' ? 'exhibition-gameCard': 'gameCard'}`}
+//       style={{ backgroundColor: gameMode.bgColor }}
 
-    >
-      <div className='card-top-row'>
-      <div className='categoryCardTopRow'>
-        <img
-          src={`${backendUrl}/${gameMode.icon}`}
-          className="cardIcon" alt={gameMode.name}
-        />
-      </div>
-      <p className='cardTitle'>{gameMode.displayName}</p>
-      <p className='cardInstruction'>{gameMode.description}</p>
-      </div>
-      <div className={`${gameMode.name === 'EXHIBITION' ? 'action-btn-container': 'action-btn-container1'}`}>
-          <p className='action-btn-text'>Play Now</p>
-      </div>
-    </div>
-  )
-}
+//     >
+//       <div className='card-top-row'>
+//       <div className='categoryCardTopRow'>
+//         <img
+//           src={`${backendUrl}/${gameMode.icon}`}
+//           className="cardIcon" alt={gameMode.name}
+//         />
+//       </div>
+//       <p className='cardTitle'>{gameMode.displayName}</p>
+//       <p className='cardInstruction'>{gameMode.description}</p>
+//       </div>
+//       <div className={`${gameMode.name === 'EXHIBITION' ? 'action-btn-container': 'action-btn-container1'}`}>
+//           <p className='action-btn-text'>Play Now</p>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default SelectGame;
