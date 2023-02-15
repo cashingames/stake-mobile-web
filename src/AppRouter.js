40752ee84f66fe116751f238613b99083a8d1c4f
Oyesola Ogundele <oyekunmi@gmail.com>
2023-02-15 09:35:25 +0400

    make app lighter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -20,8 +20,6 @@ import NotificationScreen from './features/NotificationScreen/NotificationScreen
 import FundWalletScreen from './features/FundWalletScreen/FundWalletScreen';
 import TransactionScreen from './features/TransactionScreen/TransactionScreen';
 import SelectGameCategoryScreen from './features/Games/SelectGameCategory/SelectGameCategoryscreen';
-import GameInstructionScreen from './features/Games/GameInstructionScreen/GameInstructionScreen';
-import ChallengeSelectPlayer from './features/Games/ChallengeSelectPlayer/ChallengeSelectPlayer';
 import GameStaking from './features/Games/GameStaking/GameStaking';
 import UserProfileScreen from './features/Profile/UserProfileScreen';
 import EditProfileDetails from './features/Profile/EditProfileDetails/EditProfileDetails';
@@ -36,57 +34,10 @@ import GameStorePurchaseFailed from './features/Store/GameStorePurchaseFailed/Ga
 import InviteFriends from './features/InviteFriends/InviteFriends';
 import SupportQuestions from './features/Support/SupportQuestions/SupportQuestions';
 import SupportAnswer from './features/Support/SupportQuestions/SupportAnswer';
-import MyChallengeScreen from './features/Games/MyChallengeScreen/MyChallengeScreen';
-import MyChallengeScore from './features/Games/MyChallengeScore/MyChallengeScore';
-import ChallengeGameInProgress from './features/Games/ChallengeGameInProgress/ChallengeGameInProgress';
-import ChallengeEndGameScreen from './features/Games/ChallengeEndGameScreen/ChallengeEndGameScreen';
-import ChallengeStaking from './features/Games/ChallengeStaking/ChallengeStaking';
-import ExtendedLeaderBoard from './features/Leaderboard/ExtendedLeaderBoard/ExtendedLeaderBoard';
 import ContactUs from './features/Support/ContactUs/ContactUs';
 import HelpPages from './features/Support/HelpPages/HelpPages';
 import AuthContactForm from './components/AuthContactUs/AuthContactUs';
 
-
-// const Login = React.lazy(() => import('./features/Auth/LoginScreen/Login'));
-// const Signup = React.lazy(() => import('./features/Auth/SignupScreen/Signup'));
-// const SignupProfile = React.lazy(() => import('./features/Auth/SignupProfileScreen/SignupProfile'));
-// const PrivacyPolicy = React.lazy(() => import('./features/Agreements/PrivacyPolicy'));
-// const TermsAndConditions = React.lazy(() => import('./features/Agreements/TermsAndConditions'));
-// const ForgotPassword = React.lazy(() => import('./features/Auth/ForgotPassword/ForgotPassword'));
-// const ResetPassword = React.lazy(() => import('./features/Auth/ResetPassword/ResetPassword'));
-// const VerifyOtp = React.lazy(() => import('./features/Auth/VerifyOtp/VerifyOtp'));
-// const VerifyRegistrationOtp = React.lazy(() => import('./features/Auth/VerifyRegistrationOtp/VerifyRegistrationOtp'));
-// const WalletScreen = React.lazy(() => import('./features/WalletScreen/WalletScreen'));
-// const NotificationScreen = React.lazy(() => import('./features/NotificationScreen/NotificationScreen'));
-// const FundWalletScreen = React.lazy(() => import('./features/FundWalletScreen/FundWalletScreen'));
-// const TransactionScreen = React.lazy(() => import('./features/TransactionScreen/TransactionScreen'));
-// const SelectGameCategoryScreen = React.lazy(() => import('./features/Games/SelectGameCategory/SelectGameCategoryscreen'));
-// const GameInstructionScreen = React.lazy(() => import('./features/Games/GameInstructionScreen/GameInstructionScreen'));
-// const ChallengeSelectPlayer = React.lazy(() => import('./features/Games/ChallengeSelectPlayer/ChallengeSelectPlayer'));
-// const LandingPage = React.lazy(() => import('./features/LandingPageScreen/LandingPage'));
-// const GameStaking = React.lazy(() => import('./features/Games/GameStaking/GameStaking'));
-// const UserProfileScreen = React.lazy(() => import('./features/Profile/UserProfileScreen'));
-// const EditProfileDetails = React.lazy(() => import('./features/Profile/EditProfileDetails/EditProfileDetails'))
-// const ChangePassword = React.lazy(() => import('./features/Profile/ChangePasswordScreen/ChangePassword'));
-// const BankDetails = React.lazy(() => import('./features/Profile/BankDetails/BankDetails'));
-// const GameInProgress = React.lazy(() => import('./features/Games/GameInProgress/GameInProgress'));
-// const GameEnded = React.lazy(() => import('./features/Games/GameEnded/GameEnded'));
-// const ReviewStake = React.lazy(() => import('./features/Games/ReviewStakeScreen/ReviewStake'));
-// const Store = React.lazy(() => import('./features/Store/Store'));
-// const GameBoostPurchaseSuccess = React.lazy(() => import('./features/Store/GameBoostPurchaseSuccess/GameBoostPurchaseSuccess'));
-// const GameStorePurchaseFailed = React.lazy(() => import('./features/Store/GameStorePurchaseFailed/GameStorePurchaseFailed'));
-// const InviteFriends = React.lazy(() => import('./features/InviteFriends/InviteFriends'));
-// const SupportQuestions = React.lazy(() => import('./features/Support/SupportQuestions/SupportQuestions'));
-// const SupportAnswer = React.lazy(() => import('./features/Support/SupportQuestions/SupportAnswer'));
-// const MyChallengeScreen = React.lazy(() => import('./features/Games/MyChallengeScreen/MyChallengeScreen'));
-// const MyChallengeScore = React.lazy(() => import('./features/Games/MyChallengeScore/MyChallengeScore'));
-// const ChallengeGameInProgress = React.lazy(() => import('./features/Games/ChallengeGameInProgress/ChallengeGameInProgress'));
-// const ChallengeEndGameScreen = React.lazy(() => import('./features/Games/ChallengeEndGameScreen/ChallengeEndGameScreen'));
-// const ChallengeStaking = React.lazy(() => import('./features/Games/ChallengeStaking/ChallengeStaking'))
-// const ExtendedLeaderBoard = React.lazy(() => import('./features/Leaderboard/ExtendedLeaderBoard/ExtendedLeaderBoard'))
-// const ContactUs = React.lazy(() => import('./features/Support/ContactUs/ContactUs'))
-// const HelpPages = React.lazy(() => import('./features/Support/HelpPages/HelpPages'))
-// const AuthContactForm = React.lazy(() => import('./components/AuthContactUs/AuthContactUs'))
 
 const AppRouter = () => {
 
@@ -123,9 +74,6 @@ const AppRouter = () => {
                 path="/select-category"
                 element={<AuthRoute redirectTo="/"><SelectGameCategoryScreen /></AuthRoute>} />
             <Route
-                path="/game-instructions"
-                element={<AuthRoute redirectTo="/"><GameInstructionScreen /></AuthRoute>} />
-            <Route
                 path="/exhibition-staking"
                 element={<AuthRoute redirectTo="/"><GameStaking /></AuthRoute>} />
             <Route
@@ -134,11 +82,6 @@ const AppRouter = () => {
             <Route
                 path="/game-result"
                 element={<AuthRoute redirectTo="/"><GameEnded /></AuthRoute>} />
-
-            <Route
-                path="/select-player"
-                element={<AuthRoute redirectTo="/"><ChallengeSelectPlayer /></AuthRoute>} />
-
             <Route
                 path="/profile"
                 element={<AuthRoute redirectTo="/"><UserProfileScreen /></AuthRoute>} />
@@ -183,24 +126,6 @@ const AppRouter = () => {
             <Route
                 path="/contact-us"
                 element={<AuthRoute redirectTo="/"><ContactUs /></AuthRoute>} />
-            <Route
-                path="/challenges"
-                element={<AuthRoute redirectTo="/"><MyChallengeScreen /></AuthRoute>} />
-            <Route
-                path="/challenge-score/:id"
-                element={<AuthRoute redirectTo="/"><MyChallengeScore /></AuthRoute>} />
-            <Route
-                path="/challenge-game"
-                element={<AuthRoute redirectTo="/"><ChallengeGameInProgress /></AuthRoute>} />
-            <Route
-                path="/challenge-result"
-                element={<AuthRoute redirectTo="/"><ChallengeEndGameScreen /></AuthRoute>} />
-            <Route
-                path="/challenge-staking"
-                element={<AuthRoute redirectTo="/"><ChallengeStaking /></AuthRoute>} />
-            <Route
-                path="/leaderboards"
-                element={<AuthRoute redirectTo="/"><ExtendedLeaderBoard /></AuthRoute>} />
             <Route
                 path="/games/staking/loading"
                 element={<AuthRoute redirectTo="/"><GameLoading /></AuthRoute>} />
