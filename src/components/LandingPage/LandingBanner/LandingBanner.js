f3754b7b312609ea4ab50d34aaa21ff8077fcfab
Aruna Joy <arunajoy2602@gmail.com>
2023-04-28 20:25:39 +0400

    fixing store links bugs

--- src/components/LandingPage/LandingBanner/LandingBanner.js
+++ src/components/LandingPage/LandingBanner/LandingBanner.js
@@ -8,10 +8,10 @@ function LandingBanner() {
       <div className='bannerTextContainer'>
         <h2 className='bannerText'><span className='red'>Think you have what it takes </span> <br />to win big? <br />Put your skills to the test <br />with our thrilling staking games</h2>
         <div className='stores-icon'>
-          <Link to="https://play.google.com/store/apps/details?id=com.cashingames.cashingames">
-            <img width="100px" src="/images/googleplay_icon.png" alt="logo" height="40px" className="social-img" /></Link>
-          <Link to="https://apps.apple.com/ng/app/cashingames/id6443878628">
-            <img width="100px" height="30px" src="/images/apple_store_icon.png" alt="logo" className="social-img" /></Link>
+          <a href="https://play.google.com/store/apps/details?id=com.cashingames.cashingames">
+            <img width="100px" src="/images/googleplay_icon.png" alt="logo" height="40px" className="social-img" /></a>
+          <a href="https://apps.apple.com/ng/app/cashingames/id6443878628">
+            <img width="100px" height="30px" src="/images/apple_store_icon.png" alt="logo" className="social-img" /></a>
         </div>
         <p className='bannerText2'>Show you are the true <br /> champion today!</p>
         <div className='clipContainer'>
