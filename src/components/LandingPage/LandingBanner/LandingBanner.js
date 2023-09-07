4500903d99663ba50d8d1ded980284f2d71a3882
Zubby Uwajimgba <zubbybrightson@gmail.com>
2023-09-07 22:45:56 +0100

    footer

--- src/components/LandingPage/LandingBanner/LandingBanner.js
+++ src/components/LandingPage/LandingBanner/LandingBanner.js
@@ -6,30 +6,16 @@ function LandingBanner() {
     <div className='landingBanner'>
       <div className='banner__container-img'>
         <img src='/images/banner-img.png' alt='banner' className='banner__image' />
-        {/* <img src='/images/mobile-banner-image.png' alt='banner' className='mobile_banner_image' /> */}
+        <img src='/images/mobile-banner-image.png' alt='banner' className='mobile_banner_image' />
       </div>
       <div className="banner__container-question">
-        <p className='banner__text-title'>Why Play Games on Cashingames</p>
         <div className='banner__container-answer'>
-          <AnswerSection img='/images/members.png' text1='90 Million +' text2='Games' />
-          <AnswerSection img='/images/safe.png' text1='100% Safe' text2='& secure' />
-          <AnswerSection img='/images/c-service.png' text1='24/7' text2='Support'  />
-          <AnswerSection img='/images/payment.png' text1='Instant' text2='Withdrawals'  />
+          <img src='/images/why_play_desktop.png' alt='banner' className='why_play_desktop' />
+          <img src='/images/why_play_mobile.png' alt='banner' className='why_play_mobile' />
         </div>
       </div>
-      {/* <img src='/images/banner-bg.png' alt='banner' className='banner__image-bg' /> */}
     </div>
   )
 }
 
 export default LandingBanner
-
-const AnswerSection = ({ img, text1, text2}) => {
-  return (
-    <div className='banner__answer'>
-      <img src={img} alt='banner' className='banner__icon' />
-      <p className='banner__text'>{text1}</p>
-      <p className='banner__text-small'>{text2}</p>
-    </div>
-  )
-}
--- src/components/LandingPage/LandingFooter/LandingFooter.js
+++ src/components/LandingPage/LandingFooter/LandingFooter.js
@@ -5,35 +5,64 @@ import './LandingFooter.scss'
 
 function LandingFooter() {
     return (
-        <div className='footer__container'>
-            <div className='footer__link-container'>
-                <button className='link-container'>
-                    <Link to="/sign-up" className='link'>Sign Up</Link>
-                </button>
-                <div className='footer__container-text'>
-                    <p className='footer__text-title'>Ready to claim your football fortune? </p>
-                    <p className='footer__text'>Sign up now and join the ranks of Cashingames champions!</p>
+        <div>
+            <div className='footer__container'>
+                <div className='footer__link-container'>
+                    <button className='link-container'>
+                        <Link to="/sign-up" className='link'>Sign Up</Link>
+                    </button>
+                    <div className='footer__container-text'>
+                        <p className='footer__text-title'>Ready to claim your football fortune? </p>
+                        <p className='footer__text'>Sign up now and join the ranks of Cashingames champions!</p>
+                    </div>
+                </div>
+                <div className='footer__container2' style={{ background: "url('/images/footer-bg2.png')" }}>
+                    <div className='footer__container-img'>
+                        <img src='/images/cs-logo.png' alt='logo' className='footer2__image' />
+                        <img src='/images/playstore.png' alt='playstore' className='footer2__image' />
+                        <img src='/images/ios.png' alt='logo' className='footer2__image' />
+                    </div>
+                    <div className='footer__newsletter'>
+                        <p className='newsletter_text'>Newsletter</p>
+                        <form className='newsletter__form'>
+                            <input />
+                        </form>
+                        <button>Sign Up</button>
+                    </div>
+                    <hr />
+                    <div className='terms' style={{ color: "#fff", fontSize: '.6rem', textAlign: 'center', fontFamily: 'gotham-medium' }}>
+                        <p className='terms__text'>© Copyright 2023 | Cashingames</p>
+                    </div>
                 </div>
             </div>
-            <div className='footer__container2' style={{ background: "url('/images/footer-bg2.png')" }}>
-                <div className='footer__container-img'>
-                    <img src='/images/cs-logo.png' alt='logo' className='footer2__image' />
-                    <img src='/images/playstore.png' alt='playstore' className='footer2__image' />
-                    <img src='/images/ios.png' alt='logo' className='footer2__image' />
+
+            <div className='mobile_landing_cta'>
+                <p className='cta_question' style={{ marginTop: '.6rem' }}>Ready to claim your </p>
+                <p className='cta_question'>football fortune?</p>
+                <p className='cta_action'>Sign up now and join</p>
+                <p className='cta_action'>the ranks of</p>
+                <p className='cta_action'>Cashingames</p>
+                <p className='cta_action'>champions!</p>
+                <Link to="/sign-up" className='link'>Sign Up</Link>
+                <img src='/images/landing_cg_logo_footer.png' alt='logo' className='footer_logo' />
+                <div className='download_and_news'>
+                    <img src='/images/footer_shape.png' alt='logo' className='footer_shape' />
+                    <div className='footer_icons'>
+                        <img src='/images/google_play_footer.png' alt='logo' className='icons' />
+                        <img src='/images/apple_store_footer.png' alt='logo' className='icons' style={{ marginBottom: '3rem' }} />
+                    </div>
                 </div>
-                <div className='footer__newsletter'>
-                    <p className='newsletter_text'>Newsletter</p>
-                    <form className='newsletter__form'>
+                <div className='newsletter'>
+                    <form className='news_letter_form'>
                         <input />
                     </form>
-                    <button>Sign Up</button>
-                </div>
-                <hr />
-                <div className='terms' style={{color: "#fff", fontSize: '.6rem', textAlign:'center', fontFamily:'gotham-medium'}}>
-                    <p className='terms__text'>© Copyright 2023 | Cashingames</p>
+                    
+                    <Link to="/sign-up" className='f_link'>Sign Up</Link>
                 </div>
             </div>
+
         </div>
+
     )
 }
 
