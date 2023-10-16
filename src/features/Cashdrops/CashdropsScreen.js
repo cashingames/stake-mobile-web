12301661ea3bd0aaee8393d6795b8ae0e0b80063
ererobe95 <danielererobe@gmail.com>
2023-10-16 07:52:00 +0100

    using day.js to format date of cash drop winnings

--- src/features/Cashdrops/CashdropsScreen.js
+++ src/features/Cashdrops/CashdropsScreen.js
@@ -8,10 +8,12 @@ import { setCashMode, setGameMode, setGameType } from '../Games/GameSlice';
 import logToAnalytics from '../../utils/analytics';
 import { formatCurrency } from '../../utils/stringUtl';
 import LoaderScreen from '../LoaderScreen/LoaderScreen';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat'
 
 import './CashdropsScreen.scss';
-import { formattedDateHandler } from '../../utils/utils';
 
+dayjs.extend(advancedFormat) 
 const backendUrl = process.env.REACT_APP_API_ROOT_URL;
 
 export default function CashdropsScreen() {
@@ -58,6 +60,9 @@ const RenderRecentWinners = () => {
 }
 
 const RenderRecentWinner = ({ winner }) => {
+    const date = dayjs(winner.winningDate);
+    const formattedDate = date.format('MMM Do')
+
     return (
         <div className="winner-container">
             <div className="name">{winner.username}</div>
@@ -80,7 +85,7 @@ const RenderRecentWinner = ({ winner }) => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none">
                     <path d="M6.21684 7.70801C5.93339 7.70801 5.69444 7.61015 5.5 7.41444C5.30556 7.21872 5.20833 6.97914 5.20833 6.69569C5.20833 6.41223 5.30619 6.17329 5.50191 5.97884C5.69762 5.7844 5.9372 5.68718 6.22066 5.68718C6.50411 5.68718 6.74306 5.78503 6.9375 5.98075C7.13194 6.17646 7.22917 6.41605 7.22917 6.6995C7.22917 6.98295 7.13131 7.2219 6.93559 7.41634C6.73988 7.61079 6.5003 7.70801 6.21684 7.70801ZM1.875 9.16634C1.70833 9.16634 1.5625 9.10384 1.4375 8.97884C1.3125 8.85384 1.25 8.70801 1.25 8.54134V2.08301C1.25 1.91634 1.3125 1.77051 1.4375 1.64551C1.5625 1.52051 1.70833 1.45801 1.875 1.45801H2.55208V0.833008H3.22917V1.45801H6.77083V0.833008H7.44792V1.45801H8.125C8.29167 1.45801 8.4375 1.52051 8.5625 1.64551C8.6875 1.77051 8.75 1.91634 8.75 2.08301V8.54134C8.75 8.70801 8.6875 8.85384 8.5625 8.97884C8.4375 9.10384 8.29167 9.16634 8.125 9.16634H1.875ZM1.875 8.54134H8.125V4.06217H1.875V8.54134ZM1.875 3.43717H8.125V2.08301H1.875V3.43717Z" fill="#1C453B" />
                 </svg>
-                <p className='date'>{formattedDateHandler(winner.winningDate)}</p>
+                <p className='date'>{formattedDate}</p>
             </div>
         </div>
     );
@@ -89,7 +94,6 @@ const RenderRecentWinner = ({ winner }) => {
 const RenderCashdrops = () => {
 
     const rounds = useSelector(selectActiveRounds);
-    console.log(rounds, 'hey');
     return (
         <div className='cashdrops-list'>
             {rounds.map((drop, i) =>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -61,32 +61,3 @@ export const setupAxios = (token, dispatch) => {
 
 }
 
-export const formattedDateHandler = (winningDate) => {
-    const date = new Date(winningDate);
-    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-    const month = months[date.getMonth()];
-    const day = date.getDate();
-
-    let daySuffix;
-    if (day >= 11 && day <= 13) {
-        daySuffix = 'th';
-    } else {
-        switch (day % 10) {
-            case 1:
-                daySuffix = 'st';
-                break;
-            case 2:
-                daySuffix = 'nd';
-                break;
-            case 3:
-                daySuffix = 'rd';
-                break;
-            default:
-                daySuffix = 'th';
-        }
-    }
-
-    const formattedDate = `${month} ${day}${daySuffix}`;
-    return formattedDate
-}
